94f0637ab1ce842c644a5427a0acc463a12f7c3e
Archit Aggarwal <47587987+architagg@users.noreply.github.com>
2021-06-04 18:10:15 +0530

    Create helprahul.cpp

---
+++ 5-SortingandSearching/helprahul.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+//Find Element
+
+int findk (int a[],int n, int key){
+	int s=0;
+	int e = n-1;
+	while(s<=e){
+		int mid = (s+e)/2;
+		if(a[mid]==key){
+			return mid;
+		}
+		else if  (a[s] <= a[mid]){
+			if(key >= a[s] && key < a[mid]){
+				e = mid-1;
+			}
+			else{
+				s = mid+1;
+			} 
+		}
+		else{
+			if(key>=a[mid] && key <= a[e]){
+				s= mid+1;
+			}
+			else{
+				e = mid-1;
+			}
+		}
+	}
+	return -1;
+}
+
+int main() {
+	int n;
+	int a[10000];
+	cin>>n;
+	for(int i=0;i<n;i++) {
+		cin>>a[i];
+	}
+	int key;
+	cin>>key;
+	cout<< findk(a,n,key);
+	return 0;
+}
