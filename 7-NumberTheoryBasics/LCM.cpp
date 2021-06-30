594be6ffe74fce451fbac85028e7a87e9a139cda
Archit Aggarwal <47587987+architagg@users.noreply.github.com>
2021-06-30 22:36:34 +0530

    Create LCM.cpp

---
+++ 7-NumberTheoryBasics/LCM.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int gcd (int a ,int b){
+	if(b==0){
+		return a;
+	}
+	else{
+		return gcd(b,a%b);
+	}
+}
+
+
+
+
+int main() {
+	int n1,n2;
+	cin>>n1>>n2;
+
+	cout<<(n1*n2)/gcd(n1,n2)<<endl;
+	return 0;
+}
