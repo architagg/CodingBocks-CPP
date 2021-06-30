399f5d74faefd1620f721351b9cd8dcd40819c15
Archit Aggarwal <47587987+architagg@users.noreply.github.com>
2021-06-30 23:21:17 +0530

    Create prateeklovescandy.cpp

---
+++ 7-NumberTheoryBasics/prateeklovescandy.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+ int p[10000055]={0};
+
+void prime_seive()
+{
+ for(int i=3;i<=10000005;i+=2)
+    {
+        p[i]=1;
+    }
+    for(int i=3;i*i<=10000005;i+=2)
+    {
+        if(p[i]==1)
+        {
+            for(int j=i*i;j<=10000005;j+=i)
+            {
+                p[j]=0;
+            }
+        }
+    }
+    p[2]=1;
+    p[1]=p[0]=0;
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+    int n;
+    cin>>n;
+   
+    prime_seive();
+    int count=0,i=2;
+    while(count<n)
+    {
+        if((p[i]==1))
+        {
+            count++;
+        }
+        i++;
+    }
+    cout<<i-1<<endl;
+	}
+return 0;
+
+}
