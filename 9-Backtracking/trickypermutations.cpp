a1c9ebde3b5a41e072b2921568ddeb9a92966dd5
Archit Aggarwal <47587987+architagg@users.noreply.github.com>
2021-07-09 19:56:34 +0530

    Create trickypermutations.cpp

---
+++ 9-Backtracking/trickypermutations.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+string ans[100000];
+int c = 0;
+
+void permute(string inp,int i)
+{
+    if(i == inp.size())
+    {
+        ans[c++] = inp;
+        return ;
+    }
+
+
+    for(int j = i;j<inp.length() ; j++)
+    {
+        swap(inp[i],inp[j]);
+        permute(inp,i+1);
+        swap(inp[i],inp[j]);   
+    }
+}
+int main() 
+{
+    string input;
+    cin>>input;
+
+    permute(input,0);
+    sort(ans,ans+c);
+
+    string temp = "";
+    for(int i = 0;i<c;i++)
+    {
+        if(temp != ans[i])
+            cout<<ans[i]<<endl;
+        temp = ans[i];
+    }
+
+    return 0;
+}
